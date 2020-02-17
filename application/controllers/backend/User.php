78dce65d2aac3c31f029ead26f9357178d3a99e7
harkiramadhan <harkiramadhan@gmail.com>
2020-02-17 18:55:16 +0700

    update

--- application/controllers/backend/User.php
+++ application/controllers/backend/User.php
@@ -24,4 +24,69 @@ class User extends CI_Controller{
     }
 
     // // // AJAX // // //
+    function table_list_user(){
+        $user = $this->M_Admin->get_AllUser()->result();
+        $no = 1;
+        foreach($user as $row){
+        ?>
+            <tr>
+                <td><?= $no++ ?></td>
+                <td><?= $row->email ?></td>
+                <td>
+                    <?php if($row->role == 1){
+                        echo "Admin Website";
+                    }elseif($row->role == 2){
+                        echo "Admin PPDB";
+                    } 
+                    ?>
+                </td>
+                <td>
+                    <div class="btn-group">
+                        <button class="btn btn-sm btn-primary edit_<?= $row->id ?>">Edit</button>
+                        <button class="btn btn-sm btn-danger  ml-1 hapus_<?= $row->id ?>" id="<?= $row->id ?>">Hapus</button>
+                    </div>
+                </td>
+            </tr>
+            <script>
+                <?php 
+                    $loader = base_url('assets/home/loader.gif');
+                ?>
+                $(document).ready(function(){
+                    $(".lihat_<?= $row->id ?>").click(function(){
+                        var iduser = this.id;
+                        var type = 'show';
+                        $.ajax({
+                            url: '<?= site_url('backend/user/modal') ?>',
+                            type: 'post',
+                            data: {iduser : iduser, type : type},
+                            beforeSend: function(){
+                                $('#modalLihat').modal('show');
+                                $('.isi').html("<div class='col-xl-12 text-center'><img src='<?= $loader ?>'></div>");
+                            },
+                            success: function(html){
+                                $('.isi').html(html);
+                            }
+                        });
+                    });
+                    $(".hapus_<?= $row->id ?>").click(function(){
+                        var iduser = this.id;
+                        var type = 'delete';
+                        $.ajax({
+                            url: '<?= site_url('backend/user/modal') ?>',
+                            type: 'post',
+                            data: {iduser : iduser, type : type},
+                            beforeSend: function(){
+                                $('#modalDelete').modal('show');
+                                $('.isiDelete').html("<div class='col-xl-12 text-center'><img src='<?= $loader ?>'></div>");
+                            },
+                            success: function(html){
+                                $('.isiDelete').html(html);
+                            }
+                        });
+                    });
+                });
+            </script>
+        <?php
+        }
+    }
 }
