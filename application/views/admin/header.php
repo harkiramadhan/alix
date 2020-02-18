5bd71d97ac744592d1fa94ce8f453a854ad75803
harkiramadhan <harkiramadhan@gmail.com>
2020-02-18 18:20:53 +0700

    update

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -40,14 +40,17 @@
         <div class="container-fluid">
             <!-- Toggler -->
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#sidenav-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+                <span class="navbar-toggler-icon"></span>
+            </button>
             <!-- User -->
             <ul class="nav align-items-center d-md-none">
                 <li class="nav-item dropdown">
                     <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right" aria-labelledby="navbar-default_dropdown_1">
                        
                     </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a target="__BLANK" href="<?= site_url('') ?>" class="btn btn-sm btn-primary"><i class="fas fa-eye"></i> Lihat Web</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -190,6 +193,9 @@
             <div class="container-fluid">
                 <!-- User -->
                 <ul class="navbar-nav align-items-center d-none d-md-flex ml-auto">
+                    <li class="nav-item dropdown">
+                        <a target="__BLANK" href="<?= site_url('') ?>" class="btn btn-sm btn-primary"><i class="fas fa-eye"></i> Lihat Web</a>
+                    </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <div class="media align-items-center">
