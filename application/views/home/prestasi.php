18f187fec8ad94262ee193497096e764bac91788
harkiramadhan <harkiramadhan@gmail.com>
2020-02-19 23:29:56 +0700

    update

--- application/views/home/prestasi.php
+++ application/views/home/prestasi.php
@@ -6,7 +6,7 @@
 <div class="page-header header-filter" data-parallax="true" style="background-image: url('<?= base_url('/assets/home/img/bg/'.$bgg->img) ?>'); height: 350px;"></div>
 <?php endif; ?>
 
-<div class="main main-raised">
+<div class="main main-raised" style="margin-top: -180px">
     <div class="profile-content">
     <div class="container">
         <div class="row">
@@ -19,10 +19,13 @@
             <div class="profile-tabs mt-2">
             <ul class="nav nav-pills nav-pills-icons justify-content-center" role="tablist">
                 <li class="nav-item">
-                    <a class="nav-link active" href="#sejarah" role="tab" data-toggle="tab">Akademik</a>
+                    <a class="nav-link active" href="#all" role="tab" data-toggle="tab">Semua</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#visi" role="tab" data-toggle="tab">Non Akademik</a>
+                    <a class="nav-link" href="#akademik" role="tab" data-toggle="tab">Akademik</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#non" role="tab" data-toggle="tab">Non Akademik</a>
                 </li>
             </ul>
             </div>
