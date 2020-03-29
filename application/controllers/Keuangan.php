997a4abe614899cfb4fd5709309d2d00b4cda964
harkiramadhan <harkiramadhan@gmail.com>
2020-03-29 17:59:04 +0700

    update

--- application/controllers/Keuangan.php
+++ application/controllers/Keuangan.php
@@ -4,12 +4,22 @@ class Keuangan extends CI_Controller{
         parent::__construct();
         if($this->session->userdata('role') != 3){
             $this->session->set_userdata('role', 3);
+            $this->session->set_userdata('email', "TEST");
             // $url = base_url();
             // redirect($url, 'refresh');
         }
     }
+
+    private function session($jenis){
+        $sess = $this->session->userdata($jenis);
+        return $sess;
+    }
+
     function index(){
-        $this->load->view('keu/header');
+        $data['title'] = "Dashboard Kwitansi Online Al Hikmah";
+        $data['email'] = $this->session('email');
+
+        $this->load->view('keu/header', $data);
         $this->load->view('keu/dashboard');
         $this->load->view('keu/footer');
     }
